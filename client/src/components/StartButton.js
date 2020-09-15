5061c5a54ea78d66ca7474b002b60359a5c133e5
ZXY101 <xzxy101x@gmail.com>
2020-09-15 19:00:40 +0200

    fix thing

--- client/src/components/StartButton.js
+++ client/src/components/StartButton.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function StartButton({ callBack, gameStarted }) {
 	return (
-		<button type="button" data-testid="startButton" className="nes-btn is-primary" onClick={callBack} style={{width: "200px", marginTop: "10px"}}>
+		<div type="button" data-testid="startButton" className="nes-btn is-primary" onClick={callBack} style={{width: "200px", marginTop: "10px"}}>
 			{!gameStarted ? "Start Game" : "Restart Game"}
-		</button>
+		</div>
 	)
 }
